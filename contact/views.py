0bd635d4fb6616a513ad0a2118eae79af39a47fc
Nicolas Bellec <nicolas.bellec@ens-rennes.fr>
2018-05-02 04:16:01 +0200

    Working form + security against csrf : Doesn't send mail yet

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
 # Create your views here.
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == "POST":
+        form = forms.ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+    else:
+        form = forms.ContactForm()
+
+
+    return render(request, 'contact.html', locals())
