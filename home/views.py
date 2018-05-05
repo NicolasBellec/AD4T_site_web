a86258079bfe94ebceb45c4b5a3ba70d000f4ba8
Marie-Morgane <m-m@retroguidage.fr>
2018-05-06 01:00:21 +0200

    home content

--- home/views.py
+++ home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    title = "Académie de Danse 4 Temps"
+    description = ""
+    home_active = "active"
+    return render(request, 'home.html', {'title':title, 'description':description, 'home_active':home_active})
