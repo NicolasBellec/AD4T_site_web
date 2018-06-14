1fdaf8e902414503fb85a2888f28046dc31dddbd
Nicolas Bellec <nicolas.bellec@ens-rennes.fr>
2018-06-15 00:15:26 +0200

    Better file presentation (will deliver txt/html directly in the browser and not by download)

--- media/views.py
+++ media/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import FileResponse, Http404
+from django.http import FileResponse, Http404, HttpResponse
 import os
 from AD4T.settings import BASE_DIR
 from media.models import Media
@@ -13,13 +13,21 @@ def mediaView(request, slug):
             if request.user.is_authenticated:
                 path_file = os.path.join(BASE_DIR, 'media/'+str(file.path))
                 content_type_file = file.content_type
-                return FileResponse(open(path_file, 'rb'), content_type=content_type_file)
+
+                if 'plain' in content_type_file:
+                    return HttpResponse(open(path_file, 'r'))
+                else:
+                    return FileResponse(open(path_file, 'rb'), content_type=content_type_file)
             else:
                 raise Http404()
         else:
             path_file = os.path.join(BASE_DIR, 'media/'+str(file.path))
             content_type_file = file.content_type
-            return FileResponse(open(path_file, 'rb'), content_type=content_type_file)
+
+            if 'plain' in content_type_file:
+                return HttpResponse(open(path_file, 'r'))
+            else:
+                return FileResponse(open(path_file, 'rb'), content_type=content_type_file)
 
     except FileNotFoundError:
         raise Http404()
