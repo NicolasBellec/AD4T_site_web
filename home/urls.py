6bfc453078fa217a1f2daa386bc83251e80da3c4
Marie-Morgane <m-m@retroguidage.fr>
2018-07-08 12:00:59 +0200

    add about pages

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home_view')
+    path('', views.home, name='home_view'),
+    path('about', views.about, name='about_view'),
+    path('projects', views.projects, name='projects_view')
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,13 @@ def home(request):
     title = "Académie de Danse 4 Temps"
     description = ""
     return render(request, 'home.html', {'title':title, 'description':description})
+
+def about(request):
+    title = "À propos"
+    description = ""
+    return render(request, 'about.html', {'title':title, 'description':description})
+
+def projects(request):
+    title = "Nos projets"
+    description = ""
+    return render(request, 'projects.html', {'title':title, 'description':description})
