633ac06c4b2c4c890e8d621c4c29df21e71a9699
Amaury Behague <amaury.behague@gmail.com>
2018-08-20 20:58:59 +0200

    heroku dyno scale

--- AD4T/settings.py
+++ AD4T/settings.py
@@ -82,13 +82,14 @@ WSGI_APPLICATION = 'AD4T.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+""" REMOVING LOCAL DATABASE TO USE HEROKU
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
 
 # Password validation
@@ -152,3 +153,9 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp/email/app-mail')
 #EMAIL_HOST_PASSWORD =
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #collectstatic support
+
+# Parse database configuration from $DATABASE_URL for Heroku
+import dj_database_url
+...
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ad4t-qualif.wsgi --log-file -
