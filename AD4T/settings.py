f07eb74a740f67340c2646af79a2333b60fc1b04
Marie-Morgane <m-m@retroguidage.fr>
2018-08-22 18:19:25 +0200

    add application django-crispy-forms via pip

--- AD4T/settings.py
+++ AD4T/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog',
     'contact',
     'passes',
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     'member',
     'media',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
